3a61e73464cf4a297fe8ec28eec9613c890430d1
Annstella <annstellawangui@gmail.com>
2018-09-01 10:08:58 +0300

    add article test file and models folder

---
+++ app/models/article.py
@@ -0,0 +1,13 @@
+class Article:
+    '''
+    Article class to define Article Objects
+    '''
+
+    def __init__(self,id,name,title,description,url,urlToImage,publishedAt):
+        self.id =id
+        self.name = name
+        self.title = title
+        self.overview = overview
+        self.poster = 'https://image.tmdb.org/t/p/w500/'+ poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
