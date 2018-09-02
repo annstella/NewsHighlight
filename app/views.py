36d0826aa55d4c50e24594f11167d0adfb6d6b46
Annstella <annstellawangui@gmail.com>
2018-09-02 21:29:08 +0300

    added more categories to views.py

--- app/views.py
+++ app/views.py
@@ -10,10 +10,13 @@ def index():
     View root page function that returns the index page and its data
     '''
      # Getting business article
-    business_articles = get_articles('technology')
-    print(business_articles)
+    business_articles = get_articles('business')
+    sports_articles = get_articles('sports')
+    technology_articles = get_articles('technology')
+
+    # print(business_articles)
     title = 'Home - Welcome to The best article Highlights Online'
-    return render_template('index.html', title = title, business_articles = business_articles)
+    return render_template('index.html', title = title, business_articles = business_articles, sports_articles = sports_articles, technology_articles = technology_articles)
 
 @app.route('/article/<int:article_id>')
 def article(article_id):
