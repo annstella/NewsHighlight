6e11e81db7a26a66e430a519053e103e05f7939f
Annstella <annstellawangui@gmail.com>
2018-09-12 12:38:39 +0300

    edit navbar

--- app/request.py
+++ app/request.py
@@ -2,9 +2,9 @@ import urllib.request, json
 from .models import Articles, News
 
 # #getting api key
-# api_key = None
-# base_url = None
-# articles_url = None
+api_key = None
+base_url = None
+articles_url = None
 
 def configure_request(app):
     global api_key, base_url, articles_url
--- config.py
+++ config.py
@@ -9,11 +9,6 @@ class Config:
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
 
 
-
-
-
-
-
 class ProdConfig(Config):
     '''
     Production  configuration child class
