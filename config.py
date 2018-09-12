b232f52a0cff364bb718464c4edc05145261d7a4
Annstella <annstellawangui@gmail.com>
2018-09-12 16:38:36 +0300

    heroku deployment

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     ALL_NEWS_API_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
-
+    SECRET_KEY=os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export NEWS_API_KEY='e078d07c8d404420a941c339c138e2be'
-
+export SECRET_KEY='12345'
 python3.6 manage.py server
